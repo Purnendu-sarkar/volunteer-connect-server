d1247423cd7d7c118fc6f3478ea7947df54d3dc9
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-12-23 21:35:25 +0600

    Add endpoint to handle new volunteer post creation

--- index.js
+++ index.js
@@ -27,13 +27,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const databasesList = await client.db('volunteerNetwork');
+    const collectionsList = await databasesList.collection('volunteerList');
+
+    // Define an endpoint to handle adding a new volunteer post to the database
+    app.post("/addPost", async (req, res) => {
+      const post = req.body;
+      const result = await collectionsList.insertOne(post);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
