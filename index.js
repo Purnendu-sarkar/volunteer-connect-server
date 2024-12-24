67661388282b4d93f8a97358e7f951113c186348
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-12-24 11:56:42 +0600

    Add endpoint to fetch a single volunteer post by ID.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -46,6 +46,15 @@ async function run() {
       const query = title ? { title: { $regex: title, $options: "i" } } : {};
       const posts = await collectionsList.find(query).toArray();
       res.send(posts);
+    });
+
+
+    // Fetch a single post by ID from the database 
+    app.get("/volunteerPost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const post = await collectionsList.findOne(query);
+      res.send(post);
     });
 
 
